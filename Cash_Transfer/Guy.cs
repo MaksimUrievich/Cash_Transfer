f6b6fcf676582390c8540983f4d9add2e738f156
maksimburda95 <maksimburda95@gmail.com>
2023-10-26 23:24:45 +0300

    Добавьте файлы проекта.

---
+++ Cash_Transfer/Guy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cash_Transfer
+{
+    internal class Guy
+    {
+        public string name;
+        public int cash;
+
+        /// <summary>
+        /// Выводит значение полей Name и Cash на консоль.
+        /// </summary>
+        public void WriteMyinfo() 
+        {
+            Console.WriteLine(name + " has " + cash + "bucks.");
+        }
+        /// <summary>
+        /// Выдаёт часть денег, удаляет их и кармана (или выводит сообщение, что денег недостаточно)
+        /// </summary>
+        /// <param name="amount"> Выдаваемая сумма </param>
+        /// <returns>
+        /// Сумма денег, взятая из кармана или 0, если денег не хватает (Или ошибку, если сумма недействительна)
+        /// </returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(name + " says: " + amount + " isn't a valid amount");
+                return 0;
+            }
+            if( amount > cash)
+            {
+                Console.WriteLine(name + " says: " + " I don't have enough cash to give you" + amount);
+                return 0;
+            }
+            cash -= amount;
+            return amount;
+        }
+        /// <summary>
+        /// Получает деньги, добавляя их в карман (Или вывыоди сообщене о недействительной сумме)
+        /// </summary>
+        /// <param name="amount"> Получаемая сумма </param>
+        public void ReceiveCash(int amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine(name + " says: " + amount + " isn't an amount I'll take");
+            }
+            else
+            {
+                cash += amount;
+            }
+        }
+    }
+}
---
+++ Cash_Transfer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cash_Transfer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy joe = new Guy() { name = "Joe", cash = 50 };
+            Guy bob = new Guy() { name = "Bob", cash =100 };
+            while (true) 
+            {
+                joe.WriteMyinfo();
+                bob.WriteMyinfo();
+
+                Console.Write("Enter an amount: ");
+                string howMuch = Console.ReadLine();
+                if (howMuch == "") return;
+                if (int.TryParse(howMuch, out int newCash))
+                {
+                    Console.Write("Who should give the cash: ");
+                    string whichGui = Console.ReadLine();
+                    if( whichGui == "Joe")
+                    {
+                        int cashGive = joe.GiveCash(newCash);
+                        bob.ReceiveCash(cashGive);
+                    }
+                    else if( whichGui == "Bob" )
+                    {
+                        int cashGive = bob.GiveCash(newCash);
+                        joe.ReceiveCash(cashGive);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Plase enter an amount ( or a blank line to exit).");
+                    }
+                }
+            }
+        }
+    }
+}
